bd287d18b727e1bb8ad85f9a5630f36b17d36d22
Nyllise Graham <ngrams@wifi-roaming-128-4-141-129.wifi.udel.edu>
2023-08-30 16:01:56 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App(): JSX.Element {
     return (
+        /*Nyllise Graham*/
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
