0400220a1c67539f1c91f44a138374512bba1fff
Nyllise Graham <blu@udel.edu>
2023-09-10 14:05:49 -0400

    Committing my changes for functions.ts.

--- src/functions.ts
+++ src/functions.ts
@@ -1,10 +1,11 @@
+/* eslint-disable no-extra-parens */
 /**
  * Consumes a single temperature in Fahrenheit (a number) and converts to Celsius
  * using this formula:
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return (temperature - 32) * (5 / 9);
 }
 
 /**
@@ -12,7 +13,11 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum = 0;
+    first > 0 ? (sum += first) : (sum += 0);
+    second > 0 ? (sum += second) : (sum += 0);
+    third > 0 ? (sum += third) : (sum += 0);
+    return sum;
 }
 
 /**
@@ -20,7 +25,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +33,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?");
 }
 
 /**
@@ -37,5 +42,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word === "yes" || word === "YES") {
+        return true;
+    } else if (word === "no" || word === "NO") {
+        return false;
+    } else {
+        return null;
+    }
 }
